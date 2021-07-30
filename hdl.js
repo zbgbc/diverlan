816d1b6178b63d0e247376b7cdb160e8b762c65d
zbgbc <61262171+zbgbc@users.noreply.github.com>
2021-07-31 01:59:48 +0800

    Update hdl.js

--- hdl.js
+++ hdl.js
@@ -61,22 +61,22 @@ function init() {
   }
   getdata = (key) => {
     if (isSurge()) return $persistentStore.read(key)
-    if (isQuanX()) return $prefs.valueForKey(key)
+    if (true) return $prefs.valueForKey(key)
   }
   setdata = (key, val) => {
     if (isSurge()) return $persistentStore.write(key, val)
-    if (isQuanX()) return $prefs.setValueForKey(key, val)
+    if (true) return $prefs.setValueForKey(key, val)
   }
   msg = (title, subtitle, body) => {
     if (isSurge()) $notification.post(title, subtitle, body)
-    if (isQuanX()) $notify(title, subtitle, body)
+    if (true) $notify(title, subtitle, body)
   }
   log = (message) => console.log(message)
   get = (url, cb) => {
     if (isSurge()) {
       $httpClient.get(url, cb)
     }
-    if (isQuanX()) {
+    if (true) {
       url.method = 'GET'
       $task.fetch(url).then((resp) => cb(null, {}, resp.body))
     }
@@ -85,7 +85,7 @@ function init() {
     if (isSurge()) {
       $httpClient.post(url, cb)
     }
-    if (isQuanX()) {
+    if (true) {
       url.method = 'POST'
       $task.fetch(url).then((resp) => cb(null, {}, resp.body))
     }
@@ -94,7 +94,7 @@ function init() {
     if (isSurge()) {
       $httpClient.put(url, cb)
     }
-    if (isQuanX()) {
+    if (true) {
       url.method = 'PUT'
       $task.fetch(url).then((resp) => cb(null, {}, resp.body))
     }
